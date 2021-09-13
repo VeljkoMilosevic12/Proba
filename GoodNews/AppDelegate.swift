74401e1b75d1e01df241e03c8247d97f07da45c5
Veljko <veljko.milosevic@ezderm.com>
2021-09-13 15:47:48 +0200

    App Delegate

--- GoodNews/AppDelegate.swift
+++ GoodNews/AppDelegate.swift
@@ -11,6 +11,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 9beb4a9 (App Delegate)
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let coloredAppearance = UINavigationBarAppearance()
